4920adafcb69ca9db1a511ee58cf3a3aa43e9b4d
Pierdomenico Duttilo <pierdomenico.duttilo@unipd.com>
2025-04-24 13:15:54 +0200

    dd

--- R/utilities.R
+++ R/utilities.R
@@ -188,10 +188,6 @@ plot_cmgnd <- function(x, parameters, model) {
 #'
 #' @export
 hist_cmgnd <- function(x, parameters, bins = 80) {
-  if (getRversion() >= "2.15.1") {
-    utils::globalVariables(c('..density..'))
-  }
-
   if (any(class(parameters) == "list")) {
     parameters <- parameters$parameters
   } else {
